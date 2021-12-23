9a77afb2af9724a6cf033302361ad2506f3fc8c6
rstar <rstar0823@icloud.com>
2021-12-23 21:11:52 +0900

    feat: Add a kaltes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'kaltes/new'
+  get 'kaltes/show'
+  get 'kaltes/edit'
   root to: 'customers#index'
   resources :customers, except: [:delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
