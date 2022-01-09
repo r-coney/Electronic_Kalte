132749e5c81c7b2776c813f6addd6b6437f33ac1
rstar <rstar0823@icloud.com>
2022-01-09 19:03:32 +0900

    feat: Add a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "admin",
+  phone: "01000000000",
+  email: "admin@example.com",
+  admin: true,
+  password: "password",
+  password_confirmation: "password"
+)
+
+50.times do |n|
+  User.create!(
+    name: "サンプルスタッフ#{n + 1}",
+    phone: "01000000000",
+    email: "sample#{n + 1}@example.com",
+    admin: "false",
+    password: "password",
+    password_confirmation: "password",
+  )
+end
+
+50.times do |n|
+  Customer.create!(
+    name: "サンプル顧客#{n + 1}",
+    gender: 1,
+    birthday: "2000-01-10",
+    phone: "02000000000",
+    address: "日本"
+  )
+end
+
+Customer.all.each do |customer|
+  customer.kaltes.create!(
+    menu: "サンプルカルテ",
+    request: "サンプルデータ",
+    menu_description: "サンプルのカルテです。",
+    note: "サンプル"
+  )
+end
